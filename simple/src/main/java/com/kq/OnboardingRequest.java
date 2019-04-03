08e275c81f09940ce1335c7f9b0413ee72602d5b
king <kongq1983@qq.com>
2019-04-03 23:38:33 +0800

    add demo

--- simple/src/main/java/com/kq/OnboardingRequest.java
+++ simple/src/main/java/com/kq/OnboardingRequest.java
@@ -1,9 +1,15 @@
 package com.kq;
 
 
+import java.text.ParseException;
+
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngineConfiguration;
+import org.activiti.engine.RepositoryService;
 import org.activiti.engine.impl.cfg.StandaloneProcessEngineConfiguration;
+import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.repository.ProcessDefinition;
+
 
 /**
  * OnboardingRequest
@@ -11,8 +17,9 @@ import org.activiti.engine.impl.cfg.StandaloneProcessEngineConfiguration;
  * @author kq
  * @date 2019/4/3
  */
+
 public class OnboardingRequest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         ProcessEngineConfiguration cfg = new StandaloneProcessEngineConfiguration()
                 .setJdbcUrl("jdbc:h2:mem:activiti;DB_CLOSE_DELAY=1000")
                 .setJdbcUsername("sa")
@@ -23,5 +30,15 @@ public class OnboardingRequest {
         String pName = processEngine.getName();
         String ver = ProcessEngine.VERSION;
         System.out.println("ProcessEngine [" + pName + "] Version: [" + ver + "]");
+
+        RepositoryService repositoryService = processEngine.getRepositoryService();
+        Deployment deployment = repositoryService.createDeployment()
+                .addClasspathResource("onboarding.bpmn20.xml").deploy();
+        ProcessDefinition processDefinition = repositoryService.createProcessDefinitionQuery()
+                .deploymentId(deployment.getId()).singleResult();
+        System.out.println(
+                "Found process definition ["
+                        + processDefinition.getName() + "] with id ["
+                        + processDefinition.getId() + "]");
     }
 }
