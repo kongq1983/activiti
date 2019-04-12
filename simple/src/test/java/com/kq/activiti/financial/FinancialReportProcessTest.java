aa1fb7dc62a0fdd91d708f3c9c322dbc38183066
king <king@qq.com>
2019-04-12 15:20:20 +0800

    demo

--- simple/src/test/java/com/kq/activiti/financial/FinancialReportProcessTest.java
+++ simple/src/test/java/com/kq/activiti/financial/FinancialReportProcessTest.java
@@ -3,13 +3,15 @@ package com.kq.activiti.financial;
 import com.kq.activiti.BaseTest;
 import org.activiti.engine.RepositoryService;
 import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
 import org.activiti.engine.repository.Deployment;
 import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
 import org.junit.Test;
 
+import java.util.List;
+
 import static org.junit.Assert.*;
-
-
 
 
 /**
@@ -18,11 +20,11 @@ import static org.junit.Assert.*;
  * @author kq
  * @date 2019-04-12
  */
-public class FinancialReportProcessTest extends BaseTest{
+public class FinancialReportProcessTest extends BaseTest {
 
 
     @Test
-    public void testDeploy(){
+    public void testDeploy() {
         RepositoryService repositoryService = this.ar.getRepositoryService();
         Deployment deployment = repositoryService.createDeployment().name("FinancialReportProcess").tenantId("1")
                 .addClasspathResource("FinancialReportProcess.bpmn20.xml")
@@ -30,18 +32,45 @@ public class FinancialReportProcessTest extends BaseTest{
 
         assertNotNull(deployment);
 
-        System.out.println("id="+deployment.getId());
+        System.out.println("id=" + deployment.getId());
 
     }
 
     @Test
-    public void testStart(){
+    public void testStart() {
 
         RuntimeService runtimeService = this.ar.getRuntimeService();
 
         ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("financialReport");
 
-        System.out.println("processInstance="+processInstance);
+        System.out.println("processInstance=" + processInstance);
+    }
+
+    @Test
+    public void testLoadTaskByCandidateUser() {
+
+//      List<Task> tasks = this.ar.getTaskService().createTaskQuery().taskTenantId("1").taskCandidateUser("kermit").list();
+        List<Task> tasks = this.ar.getTaskService().createTaskQuery().taskCandidateUser("kermit").list();
+        System.out.println("task size = "+tasks.size());
+        for (Task t : tasks) {
+            System.out.println("id=" + t.getId() + " name=" + t.getName());
+        }
+
+
+    }
+
+
+    @Test
+    public void testLoadTaskByGroup() {
+
+        List<Task> tasks = this.ar.getTaskService().createTaskQuery().taskCandidateGroup("accountancy").list();
+
+        System.out.println("task size = "+tasks.size());
+        for (Task t : tasks) {
+            System.out.println("id=" + t.getId() + " name=" + t.getName());
+        }
+
+
     }
 
 
